deb507b5cce09d5814a67d745c79b52c2bdb8de4
Felipe Medeiros <felipeff_freitas@outlook.com>
2025-04-30 17:48:21 -0300

    Ajustando formatação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,15 @@ import HeroCarousel from '@components/sections/home/HeroCarousel';
 import ProgramaDnaBrasil from '@/components/sections/home/ProgramaDnaBrasil';
 
 const Home: React.FC = () => {
-  return (
-    <div className="relative" style={{ zIndex: 10 }}>
-      {/* Hero Carousel */}
-      <HeroCarousel />
+    return (
+        <div className="relative" style={{ zIndex: 10 }}>
+            {/* Hero Carousel */}
+            <HeroCarousel />
 
-      {/* O Programa DNA do Brasil */}
-      <ProgramaDnaBrasil />
-    </div>
-  );
+            {/* O Programa DNA do Brasil */}
+            <ProgramaDnaBrasil />
+        </div>
+    );
 };
 
 export default Home;
