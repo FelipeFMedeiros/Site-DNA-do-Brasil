d14d14c924c1a792480507f91a56f24042595ea3
Felipe Medeiros <felipeff_freitas@outlook.com>
2025-05-01 14:39:09 -0300

    fix: Resolvendo bug do scroll não voltar no início da page ao trocar de page

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
             '@assets': path.resolve(__dirname, './src/assets'),
             '@styles': path.resolve(__dirname, './src/styles'),
             '@utils': path.resolve(__dirname, './src/utils'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
         },
     },
     build: {
